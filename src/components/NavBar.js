d2efc93e40b2ba3b8fe9d168fc28aaeddcd25ead
Rodrigo Rideout <rodrigorideout@gmail.com>
2022-12-28 23:11:57 -0300

    se creo nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+    <div>
+        <nav className="navegador">
+            <a href="./index.html"> </a>
+            <ul class="cabecera_links">
+                <li>Inicio</li>
+                <li>Miembros</li>
+                <li>Discografía</li>
+                <li>Galería</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
